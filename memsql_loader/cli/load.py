2c62e1f007cc9a88c07a15deb3d03c046769db9d
Vlad777 <vlad777@gmail.com>
2016-02-11 18:19:59 -0800

    fixing job spec check.

--- memsql_loader/cli/load.py
+++ memsql_loader/cli/load.py
@@ -316,7 +316,7 @@ Invalid command line options for load:
             #header_columns = options.columns
             with pool.get_connection(database='INFORMATION_SCHEMA', **self.job.spec.connection) as conn:
                 self.job.spec.options.columns = db_utils.get_column_names(conn, self.job.spec.target.database, self.job.spec.target.table)
-                if not self.options.columns:
+                if not self.job.spec.options.columns:
                     self.logger.error("The table specified (%s) must exist", self.job.spec.target.table)
                     sys.exit(1)
 
